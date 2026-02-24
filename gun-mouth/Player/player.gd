0a6857908b7c60405a6fc8584a0cef28dfe6fb73
Lognas <42080354+lognasmeow@users.noreply.github.com>
2026-02-23 22:51:57 -0500

    add player object

---
+++ gun-mouth/Player/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+@export var mouseSensitivity := 0.5
+var rotation_x := 0.0
+var rotation_y := 0.0
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotation_y -= event.relative.x * mouseSensitivity
+		rotation_x -= event.relative.y * mouseSensitivity
+		
+		rotation_x = clamp(rotation_x, -70, 70)
+		
+		rotation_degrees.y = rotation_y
+		rotation_degrees.x = rotation_x
