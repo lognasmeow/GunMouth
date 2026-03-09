9c13e804ffad15b09289f3d23a6152c6da9d7cfe
Lognas <42080354+lognasmeow@users.noreply.github.com>
2026-03-08 20:14:51 -0400

    clean up sprinting code

--- gun-mouth/Player/player.gd
+++ gun-mouth/Player/player.gd
@@ -14,11 +14,11 @@ func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Add the gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# Handle jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
@@ -26,27 +26,20 @@ func _physics_process(delta):
 		isSprinting = true
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if input_dir.y >= 0 and isSprinting == true:
 		isSprinting = false
+		
+	var currentSpeed = SPEED * (SPRINT_MULTIPLIER if isSprinting else 1)
 	
 	if direction:
-		if isSprinting:
-			velocity.x = direction.x * (SPEED * SPRINT_MULTIPLIER)
-			velocity.z = direction.z * (SPEED * SPRINT_MULTIPLIER)
-		else:
-			velocity.x = direction.x * SPEED
-			velocity.z = direction.z * SPEED
+		velocity.x = direction.x * currentSpeed
+		velocity.z = direction.z * currentSpeed
 	else:
-		if isSprinting:
-			velocity.x = move_toward(velocity.x, 0, (SPEED * SPRINT_MULTIPLIER))
-			velocity.z = move_toward(velocity.z, 0, (SPEED * SPRINT_MULTIPLIER))
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-			velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, currentSpeed)
+		velocity.z = move_toward(velocity.z, 0, currentSpeed)
 
 	move_and_slide()
 
